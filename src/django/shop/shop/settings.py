432abc704840ff40d70e732c89ca50cd31f4ecda
Sander <hanakmumakil23@gmail.com>
2019-01-22 12:54:13 +0100

    CreateApp run in Django and database set to postgres-container

--- src/django/shop/shop/settings.py
+++ src/django/shop/shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0+zp+c0stc-8j)(kn454dspkt#&p9vp!2be3gmni4c-m3aod$3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100']
 
 
 # Application definition
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
