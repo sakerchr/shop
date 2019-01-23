94388de9425b3ee006f8a2ac005dbdc3a60e7d67
Mugthakk <mugtakk@github.com>
2019-01-23 12:01:58 +0100

    Adds urls to httpresponses for simple api

--- src/django/shop/shop_app/admin.py
+++ src/django/shop/shop_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Item, ItemCategory, Order, Review
 
 # Register your models here.
+
+admin.site.register(Item)
+admin.site.register(ItemCategory)
+admin.site.register(Order)
+admin.site.register(Review)
--- src/django/shop/shop_app/views.py
+++ src/django/shop/shop_app/views.py
@@ -4,3 +4,34 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world. You are at the shop index.")
+
+def login(request):
+    return HttpResponse("Login page.")
+
+def items(request):
+
+    return HttpResponse("Items.")
+
+def categories(request):
+
+    return HttpResponse("Categories.")
+
+def items_from_category(request, category_name):
+
+    return HttpResponse("items from category %s" % category_name)
+
+def item(request, item_name):
+
+    return HttpResponse("item. %s" % item_name)
+
+def user_orders(request):
+
+    return HttpResponse("user orders.")
+
+def reviews_for_item(request, item_name):
+
+    return HttpResponse("reviews for item %s" % item_name)
+
+def reviews_by_user(request):
+
+    return HttpResponse("reviews by user")
