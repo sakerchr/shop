564d991e903be8c4549aa8a6982fec0f68180cfe
Mugthakk <sanderac@stud.ntnu.no>
2019-01-30 11:48:57 +0100

    adds id to serializers

--- src/django/shop/shop_app/serializers.py
+++ src/django/shop/shop_app/serializers.py
@@ -5,28 +5,28 @@ from .models import *
 class ItemCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemCategory
-        fields = ("name", "description")
+        fields = ("id", "name", "description")
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ("name", "description", "number_in_stock", "price", "categories")
+        fields = ("id", "name", "description", "number_in_stock", "price", "categories")
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'groups')
+        fields = ("id", 'username', 'email', 'groups')
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ("items", "user")
+        fields = ("id", "date", "items", "user")
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ("rating", "text", "user", "item")
+        fields = ("id", "rating", "text", "user", "item")
